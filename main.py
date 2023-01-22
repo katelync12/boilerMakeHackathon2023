74aba4d3780f6539d05b1c058cbb303d82b1adbd
katelync12 <katelynchen122132@gmail.com>
2023-01-21 23:40:02 -0500

    Moved random tweets to a different page

--- main.py
+++ main.py
@@ -11,35 +11,6 @@ e = "Couldn't connect to db."
 
 app = Flask(__name__, static_folder="css")
 
-data = [
-    {
-        'rank':'1',
-        'username': '@Donuts',
-        'cancels': '7736'
-    },
-    {
-        'rank':'2',
-        'username': '@TreasureChest',
-        'cancels': '6032'
-    },
-    {
-        'rank':'3',
-        'username': '@JimmyJohns',
-        'cancels': '5132'
-    },
-    {
-        'rank':'4',
-        'username': '@Duolingo',
-        'cancels': '4211'
-    },
-    {
-        'rank':'5',
-        'username': '@SlimJim',
-        'cancels': '5131'
-    }
-]
-
-# @app.route("/<name>")
 
 @app.route("/")
 def user(name=''):
@@ -111,6 +82,77 @@ def user(name=''):
     image = get_image['data'][0]['profile_image_url']
 
     return render_template("index.html", content=retrieveList, image=image, username=username)#data)
+
+
+@app.route("/random")
+def random():
+    """
+    Workflow:
+    1) retrieve a certain number of recent tweets
+    2) save those tweets to the db and their sentiment
+    3) retrieve a certain number of recent tweets from db.
+    3)
+    :param name:
+    :return:
+    """
+
+    # Retrieve tweets
+    url = create_url()
+    try:
+        global tweets
+        tweets = [] #: List[Dict[str,str]]
+        tweets = connect_to_endpoint(url)
+    except Exception as err:
+        print(f"Error retrieving tweets: {err}")
+
+    # Save to db:
+    # for tweet in tweets:
+    #     insert("")
+
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except:
+        print(e)
+
+    cur = conn.cursor()
+    
+    # cur.execute("CREATE TABLE tweets(id, text)")
+    for tweet in tweets:
+        # tweet is the form {"id":"12312", "text":"Hello!"} tweet["id"] tweet["text"]
+        cur.execute("""
+        INSERT INTO tweets VALUES
+            (?, ?)
+        """, [tweet["id"], tweet["text"]])
+
+    conn.commit()
+    retrieve = cur.execute("SELECT id, text FROM tweets")
+    retrieveList = retrieve.fetchall()
+
+    # TODO: retrieve again
+    print('Num tweets:', len(tweets))
+    conn.close()
+
+    username = "taylorswift13"
+
+    for word in retrieveList:
+        if "@" in word[1]:
+            tweetList = word[1].split()
+            for word in tweetList:
+                if word.startswith("@"):
+                    username = word
+                    username = username.replace("@", "")
+                    username = username.replace(":", "")
+                    username = username.replace("-", "")
+                    break
+
+    image_url = get_users_with_bearer_token.create_url(username)
+    get_image = get_users_with_bearer_token.connect_to_endpoint(image_url)
+    # image = json.dumps(get_image, indent=4, sort_keys=True)
+    image = get_image['data'][0]['profile_image_url']
+
+    return render_template("random.html", content=retrieveList, image=image, username=username)#data)
+
 
 
 @app.route("/aboutUs")
